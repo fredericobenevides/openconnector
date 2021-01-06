0b82de5c8b372baa5d3ca1d66ba9b6f6eb3820ab
Frederico Benevides <fredbene@gmail.com>
2021-01-06 11:23:23 -0300

    Integrated with openconnect to connect to vpn using some default params

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+
+const openconnect = require("./lib/openconnect");
 
 clear();
 
 console.log(
   chalk.yellow(
-    figlet.textSync('OpenConnect-cli', {
-      horizontalLayout: 'full',
+    figlet.textSync("OpenConnect-cli", {
+      horizontalLayout: "full",
     })
   )
 );
+
+openconnect.connect();
